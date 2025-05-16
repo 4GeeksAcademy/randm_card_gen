c4dc923c5dfef46775ccefe8a844b4f0ce3c022a
jusspian <luisfjuspian@gmail.com>
2025-05-16 15:23:20 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,57 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ['spade','club','heart','diamond'];
+  const simbolos ={
+    'spade':'♠',
+    'club':'♣',
+    'heart':'♥',
+    'diamond':'♦'
+  };
+const valores = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+const paloValue = Math.floor(Math.random()*4);
+const chosenvalue = Math.floor(Math.random()*13);
+const palo = palos[paloValue];
+const simbolo = simbolos[palo];
+const valor = valores[chosenvalue];
+
+const divcarta = document.getElementById('carta');
+divcarta.className = `card ${palo}`;
+document.getElementById('top-left').innerHTML=`${simbolo}`;
+document.getElementById('center').innerHTML=`${valor}`;
+document.getElementById('bottom-right').innerHTML=`${simbolo}`;
 };
+
+function generarcarta (){
+
+  const palos = ['spade','club','heart','diamond'];
+  const simbolos ={
+    'spade':'♠',
+    'club':'♣',
+    'heart':'♥',
+    'diamond':'♦'
+  };
+const valores = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+const paloValue = Math.floor(Math.random()*4);
+const chosenvalue = Math.floor(Math.random()*13);
+const palo = palos[paloValue];
+const simbolo = simbolos[palo];
+const valor = valores[chosenvalue];
+
+const divcarta = document.getElementById('carta');
+divcarta.className = `card ${palo}`;
+document.getElementById('top-left').innerHTML=`${simbolo}`;
+document.getElementById('center').innerHTML=`${valor}`;
+document.getElementById('bottom-right').innerHTML=`${simbolo}`;
+
+}
+
+document.getElementById('button-generator').addEventListener('click', generarcarta);
+
+
+
+
